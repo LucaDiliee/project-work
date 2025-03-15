2e9602339502cd5d8ead2f94757333973f2b9131
LucaDiliee <luca.dilie@gsom.polimi.it>
2025-03-15 10:52:31 +0100

    Update and rename Stock_evaluator to Stock_evaluator.py

---
+++ Stock_evaluator.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import pandas as pd
+import os
+import streamlit as st
+
+"""
+# Stocks evaluator
+hi user, write a stock name
+"""
+
+ticker = st.text_input("Inserisci il ticker della stock:", "AAPL")  
+# Valore predefinito: Apple
+
+# Scarica i dati se è stato inserito un ticker 
+    
+if st.button("Scarica dati"):
+    if ticker: # Scarica i dati dell'ultimo mese
+        stock = yf.Ticker(ticker)
+        data = stock.history(period="1mo")
+        if data.empty:
+            st.error("⚠️ Nessun dato trovato. Controlla il ticker.")
+        else:
+            st.write(data)
